6cb5625d2308dbed01053a22ceb268c5ffb4bca8
levend1s <joshualevendis@Joshuas-MacBook-Air.local>
2025-12-10 16:09:28 +1100

    initial script push

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 01_m6A_3p_readthrough_analysis
+
+# 0 - SLURM script (basecalling + alignment)
+# 1 - BAM filtering
+# 2 - Generating bedmethyls
+# 3 - FeatureCounts (+ chimeric featureCounts)
+# 4 - RQC TES approximation and gene methylation analysis
+
---
+++ env.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# USER SPECIFIED
+FEATURECOUNTS_PATH="~/mount_rna/subread-2.0.6-Linux-x86_64/bin/featureCounts"
+MODKIT_PATH="modkit"
+RQC_DIR="/Users/joshualevendis/rqc"
+RQC_PATH="/Users/joshualevendis/rqc/rqc.py"
+
+ANNOTATION_FILE="/Users/joshualevendis/Documents/RNA/honours/Pfalciparum3D7/gff/data/PlasmoDB-67_Pfalciparum3D7.gff"
+GENOME_FILE="/Users/joshualevendis/Documents/RNA/honours/Pfalciparum3D7/fasta/data/PlasmoDB-67_Pfalciparum3D7_Genome.fasta"
+GENOME_FAI_FILE="/Users/joshualevendis/Documents/RNA/honours/Pfalciparum3D7/fasta/data/PlasmoDB-67_Pfalciparum3D7_Genome.fasta.fai"
+
+
+BAMDIR="/Users/joshualevendis/Downloads/bams"
+
+FILTERED_BAMDIR=$(pwd)/01_BAM_filtering_output
+
+# requirements: 28C1_to_pfal_0.95.0p.a.bed, 28C2_to_pfal_0.95.0p.a.bed, 32C1_to_pfal_0.95.0p.a.bed, 32C2_to_pfal_0.95.0p.a.bed, 36C1_to_pfal_0.95.0p.a.bed, 36C2_to_pfal_0.95.0p.a.bed
+BEDMETHYL_DIR=$(pwd)/02_generating_bedmethyls_output
+FEATURECOUNTS_DIR=$(pwd)/03_featureCounts_output
+
+TES_ANALYSIS_DIR=$(pwd)/04_tes_and_gene_methylation_analysis_output
+GLORI_X_NANOPORE_DIR=$(pwd)/05_glori_x_nanopore_overlap_output
+
+# requirements: glori-seq_12hpi.tsv, glori-seq_24hpi.tsv, glori-seq_48hpi.tsv
+GLORI_DIR=/Users/joshualevendis/Library/CloudStorage/OneDrive-TheUniversityofMelbourne/rqc_output/GLORI-seq
+
