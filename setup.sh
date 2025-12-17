94a2cecad00f78b9937614dc03beee0a8fdd5494
levend1s <joshualevendis@Joshuas-MacBook-Air.local>
2025-12-17 15:29:41 +1100

    add 09_qc_plots setup.sh and update variable names

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source ./env.sh
+
+# check dorado
+
+# check bedtools
+
+# check featureCounts
+
+git clone https://github.com/levend1s/rqc.git ${RQC_DIR}
+cd ${RQC_DIR}
+python3 -m virtualenv env
+source env/bin/activate
+pip install -r requirements.txt
+deactivate
