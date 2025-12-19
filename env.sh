ff9c3d192e3e29aff48327504c1388a0d647f669
Joshua Levendis <jlevendis@student.unimelb.edu.au>
2025-12-19 12:09:33 +1100

    updating variable names

--- env.sh
+++ env.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # USER SPECIFIED
-FEATURECOUNTS_PATH="featureCounts"
-MODKIT_PATH="modkit"
-RQC_DIR="/Users/joshualevendis/rqc"
+FEATURECOUNTS_PATH="/Users/jlevendis/Downloads/subread-2.1.1-macOS-arm64/bin/featureCounts"
+MODKIT_PATH="/Users/jlevendis/Downloads/dist_modkit_v0.6.0_68b540b/modkit"
+RQC_DIR="/Users/jlevendis/rqc"
 RQC_PATH="${RQC_DIR}/rqc.py"
 
-PLASMODB_DIR="/Users/joshualevendis/Documents/RNA/honours/Pfalciparum3D7/"
+PLASMODB_DIR="/Users/jlevendis/Downloads/Pfalciparum3D7/"
 ANNOTATION_FILE="${PLASMODB_DIR}/gff/data/PlasmoDB-67_Pfalciparum3D7.gff"
 GENOME_FILE="${PLASMODB_DIR}/fasta/data/PlasmoDB-67_Pfalciparum3D7_Genome.fasta"
 GENOME_FAI_FILE="${PLASMODB_DIR}/fasta/data/PlasmoDB-67_Pfalciparum3D7_Genome.fasta.fai"
 GAF_FILE="${PLASMODB_DIR}/gaf/PlasmoDB-67_Pfalciparum3D7_GO.gaf"
 
-BAMDIR="/Users/joshualevendis/Downloads/bams"
+BAMDIR="/Users/jlevendis/Downloads/sra_upload_bams"
 
 FILTERED_BAMDIR=$(pwd)/01_BAM_filtering_output
 
@@ -27,5 +27,5 @@ GENE_NEIGHBOUR_DIR=$(pwd)/06_gene_neighbours_output
 FEATURE_MAPPED_BEDMETHYL_DIR=$(pwd)/07_bedmethyl_feature_mapping_output
 
 # requirements: glori-seq_12hpi.tsv, glori-seq_24hpi.tsv, glori-seq_48hpi.tsv
-GLORI_DIR=/Users/joshualevendis/Library/CloudStorage/OneDrive-TheUniversityofMelbourne/rqc_output/GLORI-seq
+GLORI_DIR=/Users/jlevendis/Library/CloudStorage/OneDrive-TheUniversityofMelbourne/rqc_output/GLORI-seq
 
--- setup.sh
+++ setup.sh
@@ -3,13 +3,24 @@
 source ./env.sh
 
 # check dorado
+# https://github.com/nanoporetech/dorado
+
+# get R
+# https://posit.co/download/rstudio-desktop/
+
+# check modkit
+# https://github.com/nanoporetech/modkit/releases/download/v0.6.0/modkit_v0.6.0_u16_x86_64.tar.gz
 
 # check bedtools
+# https://formulae.brew.sh/formula/samtools
 
 # check featureCounts
+# https://sourceforge.net/projects/subread/
 
+# get rqc
 git clone https://github.com/levend1s/rqc.git ${RQC_DIR}
 cd ${RQC_DIR}
+# pip3 install virtualenv
 python3 -m virtualenv env
 source env/bin/activate
 pip install -r requirements.txt
