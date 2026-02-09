0afc49e9fa9cb6be777411b90757c45b813dcf81
Joshua Levendis <jlevendis@student.unimelb.edu.au>
2026-02-09 16:38:29 +1100

    m6A tes predictor analysis

--- R/m6A_tes_predictor_analysis.R
+++ R/m6A_tes_predictor_analysis.R
@@ -5,340 +5,228 @@ library(ggExtra)
 library(patchwork)
 library(ggpubr)
 
-path_ctrl <- "/Users/jlevendis/rqc/100nt_28C2_m6A_tes_predictor.tsv"
-path_ks <- "/Users/jlevendis/rqc/100nt_28K1_m6A_tes_predictor.tsv"
-# path <- "/Users/jlevendis/rqc/100nt0.65p_28K1_m6A_tes_predictor.tsv"
-# path <- "/Users/jlevendis/rqc/m6A_tes_predictor.tsv"
-
-# path <- "/Users/jlevendis/rqc/1p_100nt_28C1_m6A_tes_predictor.tsv"
-# path <- "/Users/jlevendis/rqc/test.tsv"
 
 
-df_ctrl <- read.delim(path_ctrl, header=TRUE)
-df_ks <- read.delim(path_ks, header=TRUE)
+args <- commandArgs(trailingOnly = TRUE)
+path <- args[1]
+timepoint <- args[2]
+out_dir <- args[3]
 
-# df <- df[df$percent_mod > 0.1,]
-df_ctrl <- df_ctrl[df_ctrl$percent_mod > 0.5,]
-df_ctrl$ID_start <- paste(df_ctrl$ID, df_ctrl$mod_start, sep = "-")
-df_ks$ID_start <- paste(df_ks$ID, df_ks$mod_start, sep = "-")
-
-df_common <- merge(df_ctrl, df_ks, by = c("ID_start"))
-# df_ks <- df_ks[df_ks$percent_mod > 0.5,]
-# df <- df[df$probability > 0.01 & df$probability < 0.99,]
+path_all <- "/Users/jlevendis/Library/CloudStorage/OneDrive-TheUniversityofMelbourne/single_molecule_m6a_analysis.tsv"
+timepoint <- "28"
+out_dir <- "lala"
 
 p_breaks <- c(0.01, 0.1, 0.5, 0.95, 0.99)
 
-# ----- histogram: percent modified ----- # 
-# ggplot(df, aes(x = percent_mod)) +
-#   geom_histogram(bins = 30, fill = "steelblue", color = "white") +
-#   theme_classic(base_size = 20) +
-#   scale_x_continuous(
-#     labels  = percent_format(accuracy = 1)
-#   ) +
-#   labs(
-#     x = "Percent modified",
-#     y = "Count"
-#   )
+df_all <- read.delim(path_all, header=TRUE)
+df_all$ID_start <- paste(df_all$ID, df_all$mod_start, sep = "-")
+df_list <- split(df_all, df_all$sample)
+
+# 28hpi
+c1_28 <- df_list[[paste(timepoint, "C1_read_depth", sep="")]]
+c2_28 <- df_list[[paste(timepoint, "C2_read_depth", sep="")]]
+k1_28 <- df_list[[paste(timepoint, "K1_read_depth", sep="")]]
+k2_28 <- df_list[[paste(timepoint, "K2_read_depth", sep="")]]
+
+c1_28 <- c1_28[c1_28$percent_mod > 0.5,]
+c2_28 <- c2_28[c2_28$percent_mod > 0.5,]
+
+common_ids <- Reduce(intersect, list(
+  c1_28$ID_start,
+  c2_28$ID_start,
+  k1_28$ID_start,
+  k2_28$ID_start
+))
+
+c1_28_common <- c1_28[c1_28$ID_start %in% common_ids, ]
+c2_28_common <- c2_28[c2_28$ID_start %in% common_ids, ]
+k1_28_common <- k1_28[k1_28$ID_start %in% common_ids, ]
+k2_28_common <- k2_28[k2_28$ID_start %in% common_ids, ]
+
+control_28_average <- merge(c1_28_common, c2_28_common, by = "ID_start", suffixes = c("_1", "_2"))
+ks_28_average <- merge(k1_28_common, k2_28_common, by = "ID_start", suffixes = c("_1", "_2"))
+
+control_28_average$weighted_avg_probability_with_mods <-
+  (control_28_average$probability_with_mods_1 * control_28_average$total_tx_with_mod_1 + 
+     control_28_average$probability_with_mods_2 * control_28_average$total_tx_with_mod_2) /
+  (control_28_average$total_tx_with_mod_1 + control_28_average$total_tx_with_mod_2)
+
+control_28_average$weighted_avg_probability_without_mods <-
+  (control_28_average$probability_without_mods_1 * control_28_average$total_tx_without_mod_1 + 
+     control_28_average$probability_without_mods_2 * control_28_average$total_tx_without_mod_2) /
+  (control_28_average$total_tx_without_mod_1 + control_28_average$total_tx_without_mod_2)
+
+ks_28_average$weighted_avg_probability_with_mods <-
+  (ks_28_average$probability_with_mods_1 * ks_28_average$total_tx_with_mod_1 + 
+     ks_28_average$probability_with_mods_2 * ks_28_average$total_tx_with_mod_2) /
+  (ks_28_average$total_tx_with_mod_1 + ks_28_average$total_tx_with_mod_2)
+
+ks_28_average$weighted_avg_probability_without_mods <-
+  (ks_28_average$probability_without_mods_1 * ks_28_average$total_tx_without_mod_1 + 
+     ks_28_average$probability_without_mods_2 * ks_28_average$total_tx_without_mod_2) /
+  (ks_28_average$total_tx_without_mod_1 + ks_28_average$total_tx_without_mod_2)
+
+# assign test groups
+
+group_c1 <- c1_28_common
+group_c2 <- c2_28_common
+group_k1 <- k1_28_common
+group_k2 <- k2_28_common
+
+group_control_average <- control_28_average
+group_ks_average <- ks_28_average
 
 
-
-# ----- histogram: probability transcript containing an m6A ends within 100nt ----- #
-# ggplot(df, aes(x = probability_with_mods)) +
-#   geom_histogram(bins = 30, fill = "steelblue", color = "white") +
-#   theme_classic(base_size = 20) +
-#   scale_x_continuous(
-#     labels  = percent_format(accuracy = 1)
-#   ) +
-#   labs(
-#     x = "probability",
-#     y = "Count"
-#   )
-# 
-# num_bins = 30
-# 
-# ggplot(df_common) +
-#   geom_histogram(
-#     aes(x = probability_with_mods.x, fill = "methylated transcripts"),
-#     bins = num_bins,
-#     alpha = 0.5,
-#     position = "identity"
-#   ) +
-#   geom_histogram(
-#     aes(x = probability_without_mods.x, fill = "unmethylated transcripts"),
-#     bins = num_bins,
-#     alpha = 0.5,
-#     position = "identity"
-#   ) +
-#   scale_fill_manual(
-#     name   = "",
-#     values = c("methylated transcripts" = "#1f77b4", "unmethylated transcripts" = "#ff7f0e")
-#   ) +
-#   scale_x_continuous(
-#     labels  = percent_format(accuracy = 1)
-#   ) +
-#   # scale_y_continuous(
-#   #   transform = "log2"
-#   # ) +
-#   theme_classic(base_size = 20) +
-#   labs(
-#     x = "Probability",
-#     y = "Count",
-#     title = "Do transcripts end within 100nt of m6A sites?"
-#   )
+# Plot 3 graphs: all samples for a timepoint
+# (methylated x unmethylated) x group: averages
+# knock sideways: averages unmethylated vs unmethylated
 
 
-# ----- scatter: num_tx_within vs probability ----- #
-# ggplot() +
-#   geom_point(data = df, aes(x = probability_with_mods, y = probability_without_mods), alpha = 1, size=1) +
-#   scale_y_continuous(
-#     # trans = logit_trans(),
-#     # breaks  = p_breaks,
-#     labels  = percent_format(accuracy = 1),
-#     name = "Probability (unmethylated transcripts)"
-#   ) +
-#   scale_x_continuous(
-#     # trans = logit_trans(),
-#     # breaks  = p_breaks,
-#     labels  = percent_format(accuracy = 1),
-#     name = "Probability (methylated transcripts)"
-#   ) +
-#   theme_classic(base_size = 20)
+# ----- histogram: control vs knocksideways all samples ----- #
 
-# # ----- 2d heatmap: probability vs percent mod (raw) ----- #
-# ggplot(df, aes(x = probability_without_mods, y = percent_mod)) +
-#   geom_bin2d(bins = 100) +
-#   scale_x_continuous(
-#     labels  = percent_format(accuracy = 1)
-#   ) +
-#   scale_y_continuous(
-#     labels  = percent_format(accuracy = 1)
-#   ) +
-#   scale_fill_viridis_c(
-#     trans = "log10",
-#     name = "num sites"
-#   ) +
-#   labs(
-#     x = "Probability transcripts end within 200nt of m6A",
-#     y = "m6A / A"
-#   ) +
-#   theme_classic(base_size = 20)
-
-# ----- 2d heatmap: probability with vs without mods (raw) ----- #
-# ggplot(df, aes(x = probability_without_mods, y = percent_mod)) +
-#   geom_bin2d(bins = 30) +
-#   scale_x_continuous(
-#     labels  = percent_format(accuracy = 1)
-#   ) +
-#   scale_y_continuous(
-#     labels  = percent_format(accuracy = 1)
-#   ) +
-#   scale_fill_viridis_c(
-#     trans = "log10",
-#     name = "num sites"
-#   ) +
-#   labs(
-#     x = "Probability (unmethylated)",
-#     y = "Probability (methylated)"
-#   ) +
-#   theme_classic(base_size = 20)
-
+alpha <- 0.3
 
 ggplot() +
   geom_density(
-    aes(x = df_common$probability_with_mods.x, fill = "methylated transcripts"),
-    alpha = 0.4,
+    aes(x = group_c1$probability_with_mods, fill = "methylated transcripts (c1)"),
+    alpha = alpha,
     linewidth = 1
   ) +
   geom_density(
-    aes(x = df_common$probability_without_mods.x, fill = "unmethylated transcripts"),
-    alpha = 0.4,
+    aes(x = group_k1$probability_with_mods, fill = "methylated transcripts (k1)"),
+    alpha = alpha,
     linewidth = 1
   ) +
-  scale_fill_manual(
-    name   = "",
-    values = c(
-      "methylated transcripts"   = "#1f77b4",
-      "unmethylated transcripts" = "#ff7f0e"
-    )
+  geom_density(
+    aes(x = group_c2$probability_with_mods, fill = "methylated transcripts (c2)"),
+    alpha = alpha,
+    linewidth = 1
   ) +
+  geom_density(
+    aes(x = group_k2$probability_with_mods, fill = "methylated transcripts (k2)"),
+    alpha = alpha,
+    linewidth = 1
+  ) +
+  geom_density(
+    aes(x = group_c1$probability_without_mods, fill = "unmethylated transcripts (c1)"),
+    alpha = alpha,
+    linewidth = 1
+  ) +
+  geom_density(
+    aes(x = group_k1$probability_without_mods, fill = "unmethylated transcripts (k1)"),
+    alpha = alpha,
+    linewidth = 1
+  ) +
+  geom_density(
+    aes(x = group_c2$probability_without_mods, fill = "unmethylated transcripts (c2)"),
+    alpha = alpha,
+    linewidth = 1
+  ) +
+  geom_density(
+    aes(x = group_k2$probability_without_mods, fill = "unmethylated transcripts (k2)"),
+    alpha = alpha,
+    linewidth = 1
+  ) +
+  scale_fill_brewer(palette = "Set1") +
+  # scale_fill_manual(
+  #   name   = "",
+  #   values = c(
+  #     "methylated transcripts"   = "#1f77b4",
+  #     "unmethylated transcripts" = "#ff7f0e"
+  #   )
+  # ) +
   scale_x_continuous(
     labels = percent_format(accuracy = 1)
   ) +
   theme_classic(base_size = 20) +
   labs(
     x = "Probability",
-    y = "Density",
-    title = "Do transcripts end within 100nt of m6A sites? 28C1"
+    y = "Density"
   )
+
+# ----- histogram: control vs knocksideways averages ----- #
 
 ggplot() +
   geom_density(
-    aes(x = df_common$probability_with_mods.y, fill = "methylated transcripts"),
-    alpha = 0.4,
+    aes(x = group_ks_average$weighted_avg_probability_with_mods, fill = "methylated transcripts (knock-sideways)"),
+    alpha = alpha,
     linewidth = 1
   ) +
   geom_density(
-    aes(x = df_common$probability_without_mods.y, fill = "unmethylated transcripts"),
-    alpha = 0.4,
+    aes(x = group_ks_average$weighted_avg_probability_without_mods, fill = "unmethylated transcripts (knock-sideways)"),
+    alpha = alpha,
     linewidth = 1
   ) +
-  scale_fill_manual(
-    name   = "",
-    values = c(
-      "methylated transcripts"   = "#1f77b4",
-      "unmethylated transcripts" = "#ff7f0e"
-    )
-  ) +
+  # scale_fill_brewer(palette = "Set2") +
+  # scale_fill_manual(
+  #   name   = "",
+  #   values = c(
+  #     "methylated transcripts (control)"   = "#1f77b4",
+  #     "unmethylated transcripts (control)" = "#ff7f0e",
+  #     "methylated transcripts (knock-sideways)" = "#1f77b4",
+  #     "unmethylated transcripts (knock-sideways)" = "#ff7f0e"
+  #   )
+  # ) +
+  scale_color_brewer(palette = "Set1") +
   scale_x_continuous(
     labels = percent_format(accuracy = 1)
   ) +
   theme_classic(base_size = 20) +
   labs(
     x = "Probability",
-    y = "Density",
-    title = "Do transcripts end within 100nt of m6A sites? 28K1"
+    y = "Density"
   )
+
+
+# ----- histogram: control vs knocksideways averages ----- #
 
 ggplot() +
   geom_density(
-    aes(x = df_common$probability_with_mods.x, fill = "control"),
-    alpha = 0.4,
+    aes(x = group_control_average$weighted_avg_probability_with_mods, fill = "methylated transcripts (control)"),
+    alpha = alpha,
     linewidth = 1
   ) +
   geom_density(
-    aes(x = df_common$probability_with_mods.y, fill = "ks"),
-    alpha = 0.4,
+    aes(x = group_control_average$weighted_avg_probability_without_mods, fill = "unmethylated transcripts (control)"),
+    alpha = alpha,
     linewidth = 1
   ) +
-  scale_fill_manual(
-    name   = "",
-    values = c(
-      "control"   = "#1f77b4",
-      "ks" = "#ff7f0e"
-    )
+  geom_density(
+    aes(x = group_ks_average$weighted_avg_probability_with_mods, fill = "methylated transcripts (knock-sideways)"),
+    alpha = alpha,
+    linewidth = 1
   ) +
+  geom_density(
+    aes(x = group_ks_average$weighted_avg_probability_without_mods, fill = "unmethylated transcripts (knock-sideways)"),
+    alpha = alpha,
+    linewidth = 1
+  ) +
+  # scale_fill_brewer(palette = "Set2") +
+  # scale_fill_manual(
+  #   name   = "",
+  #   values = c(
+  #     "methylated transcripts (control)"   = "#1f77b4",
+  #     "unmethylated transcripts (control)" = "#ff7f0e",
+  #     "methylated transcripts (knock-sideways)" = "#1f77b4",
+  #     "unmethylated transcripts (knock-sideways)" = "#ff7f0e"
+  #   )
+  # ) +
+  scale_color_brewer(palette = "Set1") +
   scale_x_continuous(
     labels = percent_format(accuracy = 1)
   ) +
   theme_classic(base_size = 20) +
   labs(
     x = "Probability",
-    y = "Density",
-    title = "Do transcripts end within 100nt of m6A sites?"
+    y = "Density"
   )
 
-ggplot() +
-  geom_density(
-    aes(x = df_common$probability_without_mods.x, fill = "control"),
-    alpha = 0.4,
-    linewidth = 1
-  ) +
-  geom_density(
-    aes(x = df_common$probability_without_mods.y, fill = "ks"),
-    alpha = 0.4,
-    linewidth = 1
-  ) +
-  scale_fill_manual(
-    name   = "",
-    values = c(
-      "control"   = "#1f77b4",
-      "ks" = "#ff7f0e"
-    )
-  ) +
-  scale_x_continuous(
-    labels = percent_format(accuracy = 1)
-  ) +
-  theme_classic(base_size = 20) +
-  labs(
-    x = "Probability",
-    y = "Density",
-    title = "Do transcripts end within 100nt of m6A sites?"
-  )
 
-wilcox.test(df_common$probability_with_mods.x, df_common$probability_with_mods.y)
-t.test(df_common$probability_with_mods.x, df_common$probability_with_mods.y)
+wilcox.test(c1_28_common$probability_with_mods, c2_28_common$probability_with_mods)
+wilcox.test(k1_28_common$probability_with_mods, k2_28_common$probability_with_mods)
+wilcox.test(c1_28_common$probability_with_mods, k1_28_common$probability_with_mods)
 
-
-# 
-# 
-# num_bins <- 30
-# 
-# p <- ggplot(df, aes(x = probability_with_mods,
-#                     y = probability_without_mods)) +
-#   geom_bin2d(bins = num_bins) +
-#   geom_point(alpha = 0) +   # 👈 invisible points for ggMarginal
-#   scale_x_continuous(
-#     labels = percent_format(accuracy = 1)
-#   ) +
-#   scale_y_continuous(
-#     labels = percent_format(accuracy = 1)
-#   ) +
-#   scale_fill_viridis_c(
-#     trans = "log10",
-#     name = "num sites"
-#   ) +
-#   labs(
-#     y = "Probability (unmethylated)",
-#     x = "Probability (methylated)"
-#   ) +
-#   guides(fill = "none") +
-#   theme_classic(base_size = 20)
-# 
-# ggMarginal(
-#   p,
-#   type = "histogram",
-#   bins = num_bins,
-#   # fill = "grey70",
-#   # color = "black"
-# )
-
-# ----- scatter: num_tx_within vs probability ----- #
-# ggplot() +
-#   geom_point(data = df, aes(x = probability_without_mods, y = percent_mod), alpha = 1, size=1) +
-#   scale_y_continuous(
-#     trans = logit_trans(),
-#     breaks  = p_breaks,
-#     labels  = percent_format(accuracy = 1),
-#     name = "m6A / A (logit)"
-#   ) +
-#   scale_x_continuous(
-#     trans = logit_trans(),
-#     breaks  = p_breaks,
-#     labels  = percent_format(accuracy = 1),
-#     name = "Probability (logit)"
-#   ) +
-#   theme_classic(base_size = 20)
-
-# ----- 2d heatmap: probability vs percent mod (logit) ----- #
-# eps <- 1e-4   # or 1e-3 if you want tighter
-# 
-# df2 <- df %>%
-#   mutate(
-#     probability_without_mods = pmin(pmax(probability_without_mods, eps), 1 - eps),
-#     probability_with_mods = pmin(pmax(probability_with_mods, eps), 1 - eps),
-#     percent_mod = pmin(pmax(percent_mod, eps), 1 - eps)
-#   )
-# 
-# ggplot(df2, aes(x = probability_with_mods, y = percent_mod)) +
-#   geom_bin2d(bins = 30) +
-#   scale_x_continuous(
-#     trans = logit_trans(),
-#     breaks  = p_breaks,
-#     labels  = percent_format(accuracy = 1),
-#     limits = c(eps, 1-eps),
-#     name = "Probability (logit)"
-#   ) +
-#   scale_y_continuous(
-#     trans = logit_trans(),
-#     breaks  = p_breaks,
-#     labels  = percent_format(accuracy = 1),
-#     limits = c(min(df$percent_mod), 1 - eps),
-#     name = "m6A / A"
-#   ) +
-#   scale_fill_viridis_c(
-#     trans = "log2",
-#     name = "num sites"
-#   ) +
-#   theme_classic(base_size = 20)
+wilcox.test(group_control_average$weighted_avg_probability_with_mods, group_ks_average$weighted_avg_probability_with_mods)
+wilcox.test(group_control_average$weighted_avg_probability_without_mods, group_ks_average$weighted_avg_probability_without_mods)
 
   
